7c71fd77f59e19396c935b29856b40c4eb9e0761
kungurtsev <kungurtsev@skbkontur.ru>
2019-08-07 18:20:38 +0500

    manual nesting modules

--- Vostok.Hercules.Client.Metrics/HerculesSinkSettingsExtensions.cs
+++ Vostok.Hercules.Client.Metrics/HerculesSinkSettingsExtensions.cs
@@ -1,5 +1,7 @@
-﻿using JetBrains.Annotations;
+﻿using System.Linq;
+using JetBrains.Annotations;
 using Vostok.Clusterclient.Core;
+using Vostok.Clusterclient.Core.Modules;
 using Vostok.Metrics;
 
 namespace Vostok.Hercules.Client.Metrics
@@ -13,7 +15,16 @@ namespace Vostok.Hercules.Client.Metrics
             settings.AdditionalSetup = setup =>
             {
                 oldSetup?.Invoke(setup);
-                setup.AddRequestModule(new MetricsModule(metricContext));
+
+                var newModule = new MetricsModule(metricContext);
+
+                var oldModules = setup.Modules.Values.SelectMany(v => v.Before);
+                var oldModule = oldModules.OfType<MetricsModule>().FirstOrDefault();
+
+                if (oldModule != null)
+                    oldModule.AddNestedModule(newModule);
+                else
+                    setup.AddRequestModule(newModule);
             };
 
             return settings;
